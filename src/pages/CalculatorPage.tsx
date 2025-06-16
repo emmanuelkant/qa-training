81a11da77faedbc6860acab541dee4ec2ae120b7
Emmanuel Kant Duarte <emmanuelkant.duarte@gmail.com>
2025-06-16 21:29:03 +0100

    add integration tests

---
+++ src/pages/CalculatorPage.tsx
@@ -0,0 +1,13 @@
+import { Calculator } from '../components/Calculator';
+
+export const CalculatorPage = () => {
+  return (
+    <div>
+      <header>
+        <h1>QA Training Platform</h1>
+        <p>This is a simple application designed to demonstrate various testing methodologies.</p>
+      </header>
+      <Calculator />
+    </div>
+  );
+}; 
---
+++ src/pages/HistoryPage.tsx
@@ -0,0 +1,13 @@
+import { History } from '../components/History';
+
+export const HistoryPage = () => {
+  return (
+    <div>
+      <header>
+        <h1>Calculation History</h1>
+        <p>View your calculation history and manage your records.</p>
+      </header>
+      <History />
+    </div>
+  );
+}; 
